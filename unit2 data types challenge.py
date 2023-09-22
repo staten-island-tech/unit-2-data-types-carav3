5af7fc8058ce0e2e8b1798ebc66bff1631bc4c3b
carav3 <carav3@nycstudents.net>
2023-09-22 14:00:32 -0400

    Update unit2 data types challenge.py

--- unit2 data types challenge.py
+++ unit2 data types challenge.py
@@ -1,4 +1,5 @@
 #challenge #1 code below
+#CHALLENGES HERE NOT COUNTING MAD LIBS PROJECT, which is in madlibs.py
 
 x = input
 
@@ -12,8 +13,28 @@ z = len(y)
 print(z)
 
 
-#challenge 2
+#challenge 2 - learning "modulo"?
+#notes below...
+# "%" and "//" are different
+# % = remainder
+# // = number of whole numbers in the answer
+#USE INT TO CONVERT IT
+
 a = input
 
 a = input("Give me a number, and I'll say if it's odd or even.")
 
+x = int(a)
+print(type(x))
+
+y = x % 2
+
+
+if y == 0:
+    print("Your number is even.")
+
+elif y > 0:
+    print("Your number is odd.")
+
+else:
+    print("Your number is odd.")
